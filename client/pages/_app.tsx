41598b9ec554cc9288cdd5ba931c26b94e03b567
KOSASIH <kosasihg88@gmail.com>
2025-05-12 11:35:16 +0700

    Create _app.tsx

---
+++ client/pages/_app.tsx
@@ -0,0 +1,24 @@
+import { WalletAdapterNetwork } from "@solana/wallet-adapter-base";
+import { WalletModalProvider } from "@solana/wallet-adapter-react-ui";
+import { PhantomWalletAdapter } from "@solana/wallet-adapter-wallets";
+import { ConnectionProvider, WalletProvider } from "@solana/wallet-adapter-react";
+import { clusterApiUrl } from "@solana/web3.js";
+import type { AppProps } from "next/app";
+import "@solana/wallet-adapter-react-ui/styles.css";
+import "../styles/globals.css";
+
+export default function App({ Component, pageProps }: AppProps) {
+  const network = WalletAdapterNetwork.Devnet;
+  const endpoint = clusterApiUrl(network);
+  const wallets = [new PhantomWalletAdapter()];
+
+  return (
+    <ConnectionProvider endpoint={endpoint}>
+      <WalletProvider wallets={wallets} autoConnect>
+        <WalletModalProvider>
+          <Component {...pageProps} />
+        </WalletModalProvider>
+      </WalletProvider>
+    </ConnectionProvider>
+  );
+}
